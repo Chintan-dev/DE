9d309fb3f680fe3ef9235f0cbf7f1be5331af05c
Chintu2011 <77568409+Chintu2011@users.noreply.github.com>
2022-04-03 04:50:41 -0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,125 @@
+
+var login = angular.module('login', []);
+
+login.controller('controller', ($scope) => {
+        $scope.login = () => {
+                $scope.form = 'form_singn_up';
+        }
+        $scope.singn_up = () => {
+                $scope.form = 'form_login';
+        }
+});
+
+
+
+
+card.run(function ($rootScope) {
+        $rootScope.globalArray = [
+                {
+                        id: '1',
+                        img: 'card.webp',
+                        name: 'Iphone 10',
+                        Rs: '95000',
+                        quantity: '1',
+                        total_quantity_mul_Rs: ''
+                }, {
+                        id: "2",
+                        img: "0.jpg",
+                        name: "Iphone 9",
+                        Rs: '85000',
+                        quantity: '1',
+                        total_quantity_mul_Rs: ''
+                }, {
+                        id: "3",
+                        img: "1.jpg",
+                        name: "Iphone 8",
+                        Rs: '75000',
+                        quantity: '1',
+                        total_quantity_mul_Rs: ''
+                }, {
+                        id: "4",
+                        img: "2.jpg",
+                        name: "Iphone 7",
+                        Rs: '75000',
+                        quantity: '1',
+                        total_quantity_mul_Rs: ''
+                }
+        ];
+        $rootScope.addedglobalArray = [];
+        $rootScope.global = [];
+});
+
+card.controller('controller1', ($scope) => {
+
+        $scope.quantity = [1, 1, 1, 1];
+        $scope.val_add = (quantity, get_id) => {
+
+                $scope.quantity[get_id - 1]++;
+                console.log("quantity:" + $scope.quantity[get_id - 1]);
+                console.log("get_id:" + get_id);
+        };
+
+
+        $scope.val_sub = (quantity, get_id) => {
+                $scope.quantity = quantity;
+                if ($scope.quantity[get_id - 1] > 1) {
+                        $scope.quantity[get_id - 1]--;
+                        console.log("quantity:" + $scope.quantity[get_id - 1]);
+                }
+
+        };
+        $scope.quantity;
+
+        $scope.card_add = (data_add, get_id, get_img, get_name, get_Rs, get_quantity, total_quantity_mul_Rs) => {
+                console.log("quantity on addcard:" + $scope.quantity[get_id - 1]);
+                // angular.forEach($scope.addedglobalArray, function (item) {
+                //         angular.forEach(item, function (value, key) {
+                //                 if (key == 'id')
+                //                         if (value == get_id) { console.log("same_id=" + value); }
+                //                 if (key == 'quantity')
+                //                         if (value == $scope.quantity[get_id - 1]) { console.log("same_q=" + value); }
+
+                //         });
+                // });
+
+                var obj = {
+                        id: get_id,
+                        img: get_img,
+                        name: get_name,
+                        Rs: get_Rs,
+                        quantity: $scope.quantity[get_id - 1],
+                        total_quantity_mul_Rs: get_Rs * $scope.quantity[get_id - 1]
+                };
+
+                $scope.addedglobalArray.push(obj);
+                console.log($scope.addedglobalArray);
+
+                // for display total
+                $scope.alltotal = '';
+                angular.forEach($scope.addedglobalArray, function (item) {
+                        angular.forEach(item, function (value, key) {
+                                if (key == 'total_quantity_mul_Rs') {
+                                        console.log(key + "=" + value);
+                                        $scope.alltotal = ($scope.alltotal * 1) + (value * 1);
+                                }
+                        });
+                });
+                console.log("alltotal find RS:" + $scope.alltotal);
+        }
+
+        $scope.del = function (data_del) {
+                $scope.addedglobalArray.splice($scope.addedglobalArray.indexOf(data_del), 1);
+                // for display total
+                $scope.alltotal = '';
+                angular.forEach($scope.addedglobalArray, function (item) {
+                        angular.forEach(item, function (value, key) {
+                                if (key == 'total_quantity_mul_Rs') {
+                                        console.log(key + "=" + value);
+                                        $scope.alltotal = ($scope.alltotal * 1) + (value * 1);
+                                }
+                        });
+                });
+                console.log("alltotal find RS:" + $scope.alltotal);
+        }
+});
+
